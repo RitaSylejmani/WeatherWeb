0046521fa2cf50e554266658fb16da63cea81850
RitaSylejmani <63064735+RitaSylejmani@users.noreply.github.com>
2020-05-24 03:02:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+let appId = '71f6779186cc32448b4c412eea65b982';
+let units = 'metric'; 
+let searchMethod; // q means searching as a string.
+
+function getSearchMethod(searchTerm) {
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
+        searchMethod = 'zip';
+    else 
+        searchMethod = 'q';
+}
+
+function searchWeather(searchTerm) {
+    getSearchMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+    });
+}
+
+function init(resultFromServer) {
+    switch (resultFromServer.weather[0].main) {
+        case 'Clear':
+            document.body.style.backgroundImage = "url('clearPicture.jpg')";
+            break;
+        
+        case 'Clouds':
+            document.body.style.backgroundImage = "url('cloudyPicture.jpeg')";
+            break;
+
+        case 'Rain':
+        case 'Drizzle':
+            document.body.style.backgroundImage = "url('rain.jpg')";
+            break;
+
+        case 'Mist':
+            document.body.style.backgroundImage = "url('mistPicture.jpg')";
+            break;    
+        
+        case 'Thunderstorm':
+            document.body.style.backgroundImage = "url('stormPicture.jpg')";
+            break;
+        
+        case 'Snow':
+            document.body.style.backgroundImage = "url('snowPicture.jpg')";
+            break;
+
+        default:
+            break;
+    }
+
+    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
+    let temperatureElement = document.getElementById('temperature');
+    let humidityElement = document.getElementById('humidity');
+    let windSpeedElement = document.getElementById('windSpeed');
+    let cityHeader = document.getElementById('cityHeader');
+
+    let weatherIcon = document.getElementById('documentIconImg');
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+
+    let resultDescription = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176;';
+    windSpeedElement.innerHTML = 'Wind Speed: ' + Math.floor(resultFromServer.wind.speed) + ' meter/s';
+    cityHeader.innerHTML = resultFromServer.name;
+    humidityElement.innerHTML = 'Humidity levels: ' + resultFromServer.main.humidity +  '%';
+
+    setPositionForWeatherInfo();
+}
+
+function setPositionForWeatherInfo() {
+    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainerHeight = weatherContainer.clientHeight;
+    let weatherContainerWidth = weatherContainer.clientWidth;
+
+    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
+    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
+    weatherContainer.style.visibility = 'visible';
+}
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+    let searchTerm = document.getElementById('searchInput').value;
+    if(searchTerm)
+        searchWeather(searchTerm);
+});
+//pjesa e login register
+function changeForm(form) {
+    let forms = document.querySelectorAll("form>div");
+    if (form == 0) {
+        forms[0].classList.remove("hidden");
+        forms[0].classList.add("form-style");
+        forms[1].classList.add("hidden");
+        forms[1].classList.remove("form-style");
+    } else {
+        forms[1].classList.remove("hidden");
+        forms[1].classList.add("form-style");
+        forms[0].classList.add("hidden");
+        forms[0].classList.remove("form-style");
+    }
+}
+
+function validate(form) {
+    let inputs = document.querySelectorAll("input");
+    if (form == 0 && inputs[0].value == "" && inputs[1].value == "") {
+        alert("to log in, you must complete the data");
+    } else if (form == 1 && inputs[3].value == "" && inputs[4].value == "" && inputs[5].value == "") {
+        alert("to register, you must complete the data");
+    }
+}
